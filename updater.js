e9126c4f36bc20c4348a4d76b03c8c57ce6e0fed
slava <slava@Dmitriys-MacBook-Air.local>
2016-05-24 15:59:36 +0300

    macos draft

--- updater.js
+++ updater.js
@@ -3,10 +3,12 @@ const fs = require('fs');
 const download = require('download-file');
 const path = require('path');
 const AdmZip = require('adm-zip');
+const request = require('request');
+const DecompressZip = require('decompress-zip');
 
 const options = {
-  url: 'http://127.0.0.1:8080',
-  zipFilePattern: 'GapminderOffline-linux-#version.zip',
+  url: 'http://192.168.1.71:8080',
+  zipFilePattern: 'GapminderOffline-darwin-#version.zip',
   cacheDir: './cache'
 };
 
@@ -30,7 +32,81 @@ function downloadUpdate(newVersion, onDownloadCompleted) {
 
   console.log(`<div>Starting download from ${url}. This operation will take some time. Wait please...</div>`);
 
+
+  /*const out = fs.createWriteStream(options.cacheDir + '/' + zipFileName);
+  const req = request({
+    method: 'GET',
+    uri: url
+  });
+  
+  req.pipe(out);
+  req.on('end', () => {
+      console.log(`<div>Download was finished</div>`);
+      console.log('<div>Starting unpack process. This operation will take some time. Wait please...</div>');
+
+      const unzipper = new DecompressZip(path.resolve(options.cacheDir, zipFileName));
+      
+      unzipper.on('error', err => {
+        console.log(`<div>Download failed from ${err}</div>`);
+        onDownloadCompleted(err);            
+      });      
+      
+      unzipper.on('extract', () => {
+        console.log('<div>Update was unpacked.</div>');
+
+        fs.chmodSync(path.resolve(options.cacheDir, 'Gapminder Offline-darwin-x64', 'updater'), '777');
+        fs.chmodSync(path.resolve(options.cacheDir, 'Gapminder Offline-darwin-x64', 'run'), '777');
+
+        console.log('<div>Now program will be restarted.</div>');
+
+        onDownloadCompleted();        
+      });
+      
+      unzipper.extract({path: options.cacheDir});
+  });*/
+
+
   download(
+    url,
+    {
+      directory: options.cacheDir,
+      filename: zipFileName
+    },
+    err => {
+      if (err) {
+        console.log(`<div>Download failed from ${err}</div>`);
+        onDownloadCompleted(err);
+        return;
+      }
+
+      console.log(`<div>Download was finished</div>`);
+      console.log('<div>Starting unpack process. This operation will take some time. Wait please...</div>');
+
+      const unzipper = new DecompressZip(path.resolve(options.cacheDir, zipFileName));
+      
+      unzipper.on('error', err => {
+        console.log(`<div>Download failed from ${err}</div>`);
+        onDownloadCompleted(err);            
+      });      
+      
+      unzipper.on('extract', () => {
+        console.log('<div>Update was unpacked.</div>');
+
+        fs.chmodSync(path.resolve(options.cacheDir, 'Gapminder Offline-darwin-x64', 'updater'), '777');
+        fs.chmodSync(path.resolve(options.cacheDir, 'Gapminder Offline-darwin-x64', 'run'), '777');
+
+        console.log('<div>Now program will be restarted.</div>');
+
+  
+      onDownloadCompleted();
+    });
+    
+    unzipper.extract({path: options.cacheDir});
+    
+    
+  });
+
+  /*download(
     url,
     {
       directory: options.cacheDir,
@@ -51,15 +127,13 @@ function downloadUpdate(newVersion, onDownloadCompleted) {
 
       console.log('<div>Update was unpacked.</div>');
 
-      fs.chmodSync(path.resolve(options.cacheDir, 'Gapminder Offline-linux-x64', 'Gapminder Offline'), '777');
-      fs.chmodSync(path.resolve(options.cacheDir, 'Gapminder Offline-linux-x64', 'libnode.so'), '777');
-      fs.chmodSync(path.resolve(options.cacheDir, 'Gapminder Offline-linux-x64', 'updater'), '777');
-      fs.chmodSync(path.resolve(options.cacheDir, 'Gapminder Offline-linux-x64', 'run'), '777');
+      fs.chmodSync(path.resolve(options.cacheDir, 'Gapminder Offline-darwin-x64', 'updater'), '777');
+      fs.chmodSync(path.resolve(options.cacheDir, 'Gapminder Offline-darwin-x64', 'run'), '777');
 
       console.log('<div>Now program will be restarted.</div>');
 
       onDownloadCompleted();
-    });
+    });*/
 }
 
 const newVersion = process.argv[2];
