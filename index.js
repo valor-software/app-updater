e6c7c7938e710a58892e5cc9ee17800b90f1fe07
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2018-10-10 14:25:05 +0300

    0.7.0 -> loadFile functionality was added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const versionCheck = require('./lib/version-check');
+const fileLoad = require('./lib/load-file');
 const download = require('./lib/download');
 const unpack = require('./lib/unpack');
 
 exports.versionCheck = versionCheck;
+exports.fileLoad = fileLoad;
 exports.download = download;
 exports.unpack = unpack;
 
